50226e7f97e548b5a66d7c860ccbd8035793ea7d
Alok Choudhary <alok.choudhary@e2enetworks.com>
2020-05-04 21:14:29 +0530

    A message containing letters from A-Z is being encoded to numbers using the following mapping:

---
+++ 91.decode-ways-recurssion.go
@@ -0,0 +1,33 @@
+// A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+// 'A' -> 1
+// 'B' -> 2
+// ...
+// 'Z' -> 26
+
+// Recurssive solution
+
+func numDecodings(s string) int {
+    result := 0
+    if string(s[0]) == "0" {
+        return 0
+    }
+    return numDecodingsUtil(s, len(s), result)
+}
+
+func numDecodingsUtil(s string, n int, result int) int {
+    if n == 0 || n == 1 {
+        return 1
+    } else {
+        result = 0
+        if string(s[n-1]) > "0" {
+            fmt.Println("first call-----", n)
+            result += numDecodingsUtil(s, n-1, result)
+        }
+        if string(s[n-2]) == "1" || (string(s[n-2]) == "2" && string(s[n-1]) < "7") {
+            fmt.Println("second call-----", n)
+            result += numDecodingsUtil(s, n-2, result)
+        }
+        return result
+    }
+}
