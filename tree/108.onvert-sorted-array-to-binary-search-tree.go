ce85f39715d95332fbfcc934cec5e855ccb985cd
Alok Choudhary <alok.choudhary@e2enetworks.com>
2020-05-09 10:12:04 +0530

    Given an array where elements are sorted in ascending order, convert it to a height balanced BST.

---
+++ tree/108.onvert-sorted-array-to-binary-search-tree.go
@@ -0,0 +1,35 @@
+// Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+// For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+c
+// Example:
+
+// Given the sorted array: [-10,-3,0,5,9],
+
+// One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+    if len(nums) == 0 {
+        return nil
+    }
+    root := new(TreeNode)
+    root.Val = nums[len(nums)/2]
+    root.Left = sortedArrayToBST(nums[:len(nums)/2])
+    root.Right = sortedArrayToBST(nums[len(nums)/2+1:])
+    return root
+}
