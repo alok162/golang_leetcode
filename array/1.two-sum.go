8b643e4d30e4d96dd9debb1a90c4741865da2d9b
Alok Choudhary <alok.choudhary@e2enetworks.com>
2020-05-10 22:15:09 +0530

    Runtime: 4 ms, faster than 92.26%

---
+++ array/1.two-sum.go
@@ -0,0 +1,21 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+func twoSum(nums []int, target int) []int {
+    dict := make(map[int]int)
+    for index, val := range nums {
+        if _, ok := dict[target-val]; ok {
+            return []int{dict[target-val], index}
+        }
+        dict[val] = index
+    }
+    return []int{}
+}
